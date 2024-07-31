5c31766d76d64e28de5d03b44bd46ff70606d03e
xoumik <shoumik21_ug@ece.nits.ac.in>
2024-08-01 03:16:02 +0530

    Portfolio

---
+++ src/data/WorkData.js
@@ -0,0 +1,60 @@
+// work data
+
+export const Work = [
+  {
+    id: 1,
+    name: "Jobify",
+    description:
+      "A dynamic and responsive MERN stack website enabling users to perform CRUD operations on job listings.",
+    tags: ["MERN", "Cloudinary"],
+
+    demo: "https://jobify-ouq0.onrender.com/",
+    github: "https://github.com/xoumik/Jobify",
+  },
+  {
+    id: 2,
+    name: "VRNexus",
+    description: "A fully responsive futuristic frontend website",
+    tags: ["ReactJs", "Tailwind-CSS"],
+
+    demo: "https://vr-nexus-sandy.vercel.app/",
+    github: "https://github.com/xoumik/VRNexus",
+  },
+  {
+    id: 3,
+    name: "BookHub",
+    description:
+      "An engaging frontend website, offering an intuitive interface for book enthusiasts.",
+    tags: ["ReactJs", "API"],
+
+    demo: "https://book-hub-orcin.vercel.app/",
+    github: "https://github.com/xoumik/BookHub",
+  },
+  {
+    id: 4,
+    name: "Sorting Visualizer",
+    description: "A cool looking website for learning sorting algorithms",
+    tags: ["NextJs", "Tailwind-CSS"],
+
+    demo: "https://sorting-visualizer-mu-hazel.vercel.app/",
+    github: "https://github.com/xoumik/sorting-visualizer",
+  },
+  {
+    id: 5,
+    name: "Travel With Me",
+    description: "Let's take a tour of the world!",
+    tags: ["HTML", "CSS", "JS"],
+
+    demo: "https://travel-with-me-snowy.vercel.app/",
+    github: "https://github.com/xoumik/TravelWithMe",
+  },
+  {
+    id: 6,
+    name: "BeatTrack",
+    description: "Will be available soon!!",
+    tags: ["ReactJs", "Tailwind-CSS", "Flask", "Gemini-AI", "ML"],
+
+    demo: "https://beat-track.vercel.app/",
+    github: "https://github.com/xoumik/BeatTrack",
+  },
+];
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import App from "./App";
+
+import "normalize.css";
+import { createRoot } from "react-dom/client";
+
+import { BrowserRouter } from "react-router-dom";
+
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
